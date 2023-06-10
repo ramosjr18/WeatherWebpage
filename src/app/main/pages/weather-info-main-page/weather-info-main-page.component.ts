8c0dd22fff77f5bd69f16f5c5c56b238f23ff074
sebas524 <104307242+sebas524@users.noreply.github.com>
2023-06-10 09:57:10 +0200

    add comments to code john weather-info

--- src/app/main/pages/weather-info-main-page/weather-info-main-page.component.ts
+++ src/app/main/pages/weather-info-main-page/weather-info-main-page.component.ts
@@ -6,12 +6,14 @@ import { MyWeatherService } from '../../services/my-weather.service';
   templateUrl: './weather-info-main-page.component.html',
   styles: [],
 })
-
 export class WeatherInfoMainPageComponent implements OnInit {
   // ! ATTRIBUTES:
+  // * timeline array will hold the time and corresponding temperature for the whole day (that means, 8 different forecasts, because it is every three hours. 8*3 = 24):
   timelineForOneDay: any = [];
   weatherNow: any;
+  // * current time will return time and date of laptop:
   currentTime = new Date();
+  // * holds city and location:
   location: any;
   latitude?: number;
   longitude?: number;
@@ -19,20 +21,25 @@ export class WeatherInfoMainPageComponent implements OnInit {
   fetched5dayWeatherData: any = [];
   HumidityDesc!: string;
   unit?: string;
+  // TODO: show error message for when city not found.
 
   // ! CONSTRUCTOR:
-  constructor(
-    public myWeatherService: MyWeatherService
-  ) { }
+  constructor(public myWeatherService: MyWeatherService) {}
 
   // ! ON INIT:
+  // * checking if geolocation is possible by browser:
+
   ngOnInit(): void {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition((position) => {
         this.latitude = position.coords.latitude;
         this.longitude = position.coords.longitude;
         this.unit = 'metric';
-        console.log(`Latitude: ${this.latitude}, Longitude: ${this.longitude}, Unit: ${this.unit}`);
+        console.log(
+          `Latitude: ${this.latitude}, Longitude: ${this.longitude}, Unit: ${this.unit}`
+        );
+
+        // * passing in geo parameters to getWeatherByGeoLocation method:
 
         this.myWeatherService
           .getWeatherByGeoLocation(this.latitude, this.longitude, this.unit)
@@ -41,6 +48,7 @@ export class WeatherInfoMainPageComponent implements OnInit {
             this.weatherData = data;
             this.getTodayForecast(this.weatherData);
             this.getFiveDayForecast(this.weatherData.list);
+            // * ! from list you get weather of every three hours
           });
       });
     } else {
@@ -49,7 +57,9 @@ export class WeatherInfoMainPageComponent implements OnInit {
   }
 
   // ! METHODS:
+  // * to compare:
   dateRange() {
+    // * it will always return a range between 2 dates, that have a difference of 3 hours -1sec: 2:59
     const start = new Date();
     start.setHours(start.getHours() + start.getTimezoneOffset() / 60);
     const to = new Date(start);
@@ -57,10 +67,16 @@ export class WeatherInfoMainPageComponent implements OnInit {
     return { start, to };
   }
 
+  // *the following method is to get the data stored in ngOnInit, it is then called inside the init with data passed in as parameter:
+
   getTodayForecast(info: any) {
     this.location = info.city;
+    // ! list is three hour forecast:
+    // * we'll look through the list and slice the data so we can get just the first 8 elements (8 elements because it has the weather for every three hours and well 8 * 3 = 24, and we are looking for *read function name):
     for (const forecast of info.list.slice(0, 7)) {
       console.log('forecast =>', forecast);
+      // * here we are pushing specific info from  list into timelineForOneDay:
+
       this.timelineForOneDay.push({
         time: forecast.dt_txt,
         temp: forecast.main.temp,
@@ -68,12 +84,18 @@ export class WeatherInfoMainPageComponent implements OnInit {
         conditions: forecast.weather![0].main,
       });
 
+      // * this const is assigned the dates provided to us by api, and then we'll change it to a timestamp:
+
       const apiDate = new Date(forecast.dt_txt).getTime();
+
+      // * if our time(within our current computer time) is within 3 hours of the time provided by us by the api
 
       if (
         this.dateRange().start.getTime() <= apiDate &&
         this.dateRange().to.getTime() >= apiDate
       ) {
+        // * if these two conditions satisfy, then we will assign data to property called weatherNow(line 13):
+
         this.weatherNow = forecast;
         console.log('weather now => ', this.weatherNow);
       }
@@ -91,6 +113,7 @@ export class WeatherInfoMainPageComponent implements OnInit {
     );
   }
 
+  // ! ---??-----
   getHumidityDesc() {
     const humidity = this.weatherNow.main.humidity;
     const messageElement = document.getElementById('humidity');
@@ -148,70 +171,67 @@ export class WeatherInfoMainPageComponent implements OnInit {
       } else {
         messageElement.textContent = 'Visibility is high.';
       }
-
     }
   }
 
+  // switchToCelcius(){
+  //   this.unit = 'metric';
+  //   console.log('this is the unit',this.unit, 'metric')
 
-  switchToCelcius(){
-    this.unit = 'metric';
-    console.log('this is the unit',this.unit, 'metric')
+  //   if (navigator.geolocation) {
+  //     navigator.geolocation.getCurrentPosition((position) => {
+  //       this.latitude = position.coords.latitude;
+  //       this.longitude = position.coords.longitude;
+  //       this.unit = 'metric';
+  //       console.log(`Latitude: ${this.latitude}, Longitude: ${this.longitude}, Unit: ${this.unit}`);
 
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition((position) => {
-        this.latitude = position.coords.latitude;
-        this.longitude = position.coords.longitude;
-        this.unit = 'metric';
-        console.log(`Latitude: ${this.latitude}, Longitude: ${this.longitude}, Unit: ${this.unit}`);
+  //       this.myWeatherService
+  //         .getWeatherByGeoLocation(this.latitude, this.longitude, this.unit)
+  //         .subscribe((data) => {
+  //           console.log('data from ngOnInit => ', data);
+  //           this.weatherData = data;
+  //           this.getTodayForecast(this.weatherData);
+  //           this.getFiveDayForecast(this.weatherData.list);
+  //         });
+  //     });
+  //   } else {
+  //     console.log('This browser does not support geolocation.');
+  //   }
+  //   const element = document.getElementById("faren")!;
+  //   const element2 = document.getElementById("celcius")!;
+  //   element.style.backgroundColor = "rgba(72, 49, 157, 0.2)";
+  //   element2.style.backgroundColor = "#49319D";
+  // }
 
-        this.myWeatherService
-          .getWeatherByGeoLocation(this.latitude, this.longitude, this.unit)
-          .subscribe((data) => {
-            console.log('data from ngOnInit => ', data);
-            this.weatherData = data;
-            this.getTodayForecast(this.weatherData);
-            this.getFiveDayForecast(this.weatherData.list);
-          });
-      });
-    } else {
-      console.log('This browser does not support geolocation.');
-    }
-    const element = document.getElementById("faren")!;
-    const element2 = document.getElementById("celcius")!;
-    element.style.backgroundColor = "rgba(72, 49, 157, 0.2)";
-    element2.style.backgroundColor = "#49319D";
-  }
+  // switchToFarenheit(){
+  //   // navigator.geolocation.getCurrentPosition((position)=>{
+  //   //   this.latitude = position.coords.latitude
+  //   //   this.longitude = position.coords.longitude
+  //   //   this.myWeatherService.getWeatherByGeoLocation(this.latitude, this.longitude, "imperial")
+  //   if (navigator.geolocation) {
+  //     navigator.geolocation.getCurrentPosition((position) => {
+  //       this.latitude = position.coords.latitude;
+  //       this.longitude = position.coords.longitude;
+  //       this.unit = 'imperial';
 
-  switchToFarenheit(){
-    // navigator.geolocation.getCurrentPosition((position)=>{
-    //   this.latitude = position.coords.latitude
-    //   this.longitude = position.coords.longitude
-    //   this.myWeatherService.getWeatherByGeoLocation(this.latitude, this.longitude, "imperial")
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition((position) => {
-        this.latitude = position.coords.latitude;
-        this.longitude = position.coords.longitude;
-        this.unit = 'imperial';
+  //       console.log(`Latitude: ${this.latitude}, Longitude: ${this.longitude}, Unit: ${this.unit}`);
 
-        console.log(`Latitude: ${this.latitude}, Longitude: ${this.longitude}, Unit: ${this.unit}`);
-
-        this.myWeatherService
-          .getWeatherByGeoLocation(this.latitude, this.longitude, this.unit)
-          .subscribe((data) => {
-            console.log('data from ngOnInit => ', data);
-            this.weatherData = data;
-            this.getTodayForecast(this.weatherData);
-            this.getFiveDayForecast(this.weatherData.list);
-          });
-      });
-    } else {
-      console.log('This browser does not support geolocation.');
-    }
-    console.log('this is the unit',this.unit, 'faren');
-    const element = document.getElementById("faren")!;
-    const element2 = document.getElementById("celcius")!;
-    element.style.backgroundColor = "#49319D";
-    element2.style.backgroundColor = "rgba(72, 49, 157, 0.2)";
-  }
+  //       this.myWeatherService
+  //         .getWeatherByGeoLocation(this.latitude, this.longitude, this.unit)
+  //         .subscribe((data) => {
+  //           console.log('data from ngOnInit => ', data);
+  //           this.weatherData = data;
+  //           this.getTodayForecast(this.weatherData);
+  //           this.getFiveDayForecast(this.weatherData.list);
+  //         });
+  //     });
+  //   } else {
+  //     console.log('This browser does not support geolocation.');
+  //   }
+  //   console.log('this is the unit',this.unit, 'faren');
+  //   const element = document.getElementById("faren")!;
+  //   const element2 = document.getElementById("celcius")!;
+  //   element.style.backgroundColor = "#49319D";
+  //   element2.style.backgroundColor = "rgba(72, 49, 157, 0.2)";
+  // }
 }
-
--- src/app/main/services/my-weather.service.ts
+++ src/app/main/services/my-weather.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
@@ -12,7 +12,7 @@ export class MyWeatherService {
 
   // ! new way
 
-  getWeatherByCityName(nameOfCity: string, unit:String) {
+  getWeatherByCityName(nameOfCity: string, unit: String) {
     const url = `${this.baseUrl}?q=${nameOfCity}&appid=${this.apiKey}&units=${unit}`;
     return this.http.get(url);
   }
@@ -23,61 +23,3 @@ export class MyWeatherService {
     return this.http.get(url);
   }
 }
-
-// // * method to generate weather
-// getWeatherForecast() {
-//   return new Observable((observer) => {
-//     // * The Geolocation.getCurrentPosition() method is used to get the current position of the device.
-//     navigator.geolocation.getCurrentPosition(
-//       (position) => {
-//         // * position here will return the coordinates.
-//         observer.next(position);
-//         console.log('POSITION', position);
-//       },
-//       (err) => {
-//         observer.next(err);
-//       }
-//     );
-//   }).pipe(
-//     // * in map we transform data. we need to append query parameters.
-//     map((val: any) => {
-//       // * remember you have already seen httpParams with fer!
-//       return (
-//         new HttpParams()
-//           .set('lon', val.coords.longitude)
-//           .set('lat', val.coords.latitude)
-//           .set('units', 'metric')
-//           // TODO: MAKE API KEY PRIVATEEEEEEE!!!!!!!!!!!!
-//           .set('appid', 'bb3a234039078f99acd82d0fce2f1c34')
-//         // .set('q', `${city}`)
-//       );
-//     }),
-//     tap((x) => {
-//       console.log('TAP HERE queryParams: ', x);
-//     }),
-//     // * switchMap will be used to fetch the data and pass it to the get method of the http:
-//     switchMap((values) => {
-//       return this.http.get(
-//         'https://api.openweathermap.org/data/2.5/forecast',
-//         { params: values }
-//       );
-//     }),
-//     tap((x) => {
-//       console.log('TAP HERE apiResponse: ', x);
-//     })
-//   );
-// }
-
-// // !METHOD X
-// getWeatherByCity(city: string): Observable<WeatherApiInterface | undefined> {
-//   const type: string = 'forecast';
-//   return this.http
-//     .get<WeatherApiInterface>(
-//       `${this.baseUrl}/${type}?q=${city}&appid=${environment.APIKEY}&units=metric`
-//     )
-//     .pipe(
-//       catchError((err) => {
-//         return of(undefined);
-//       })
-//     );
-// }
